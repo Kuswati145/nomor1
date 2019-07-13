4ac157e7fa488340130f6777764a80938392367e
Kuswati145 <52770944+Kuswati145@users.noreply.github.com>
2019-07-13 22:57:11 +0700

    Add files via upload

---
+++ 01.php
@@ -0,0 +1,25 @@
+function getProfile() {  return {
+    name: 'Kuswati',
+    age: '18' //Number,
+    address: 'Cibaregbeg RT 01 RW O5 Desa Sepatnunggal, majenang, cilacap'//Array,
+    hobbies: ['membaca', 'game'] //Array,
+    is_married: false //Boolean,
+    list_school: {
+      name: 'SMKN 1 WANAREJA',
+      years_in: '2016',
+      years_out:'2019',
+      major:'null',
+    }, // Array Of Object
+    skills: [{
+      name: 'Coding',
+      level: 'beginner',
+    },{
+      name: 'Desain grafis',
+      level: 'advanced',
+    },{
+      name: 'Editing',
+      level: 'beginner',
+    }] // Array Of Object,
+    interest_in_coding: false //Boolean,
+  }
+}
